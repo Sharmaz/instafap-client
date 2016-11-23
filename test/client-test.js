433e36cd89686a2a4dd8020b8eea9403bf7316d8
Sharmaz <irae45@gmail.com>
2016-11-23 13:30:59 -0600

    Hacemos la primera impelementación de la función getPicture, haciendo uso de nock para hacer mocking de servidor, request-promise y bluebird

--- test/client-test.js
+++ test/client-test.js
@@ -2,10 +2,23 @@
 
 const test = require('ava')
 const instafap = require('../')
-// const fixtures = require('./fixtures')
+const fixtures = require('./fixtures')
+const nock = require('nock')
+
+let options = {
+  endpoints: {
+    pictures: 'http://instafap.test/picture',
+    users: 'http://instafap.test/user',
+    auth: 'http://instafap.test/auth'
+  }
+}
+
+test.beforeEach(t => {
+  t.context.client = instafap.createClient(options)
+})
 
 test('client', t => {
-  const client = instafap.createClient()
+  const client = t.context.client
 
   t.is(typeof client.getPicture, 'function')
   t.is(typeof client.savePicture, 'function')
@@ -15,4 +28,18 @@ test('client', t => {
   t.is(typeof client.saveUser, 'function')
   t.is(typeof client.getUser, 'function')
   t.is(typeof client.auth, 'function')
+})
+
+test('getPicture', async t => {
+  const client = t.context.client
+
+  let image = fixtures.getImage()
+
+  nock(options.endpoints.pictures)
+    .get(`/${image.publicId}`)
+    .reply(200, image)
+
+  let result = await client.getPicture(image.publicId)
+
+  t.deepEqual(image, result)
 })
